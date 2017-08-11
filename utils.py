bac2f04babc663f1057d17832c425b9168036727
Ankur Tyagi <7h3rAm@gmail.com>
2017-08-11 12:48:49 -0700

    Added Python version of TallTweets' text splitting utility.

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from ttp import ttp
 import sqlite3
 import pika
 
@@ -183,6 +184,40 @@ def epoch_to_human_localtime(epoch):
 
 def epoch_to_human_utc(epoch):
   return time.strftime("%d/%b/%Y %H:%M:%S UTC", time.gmtime(epoch))
+
+# python version of TallTweets' js implementation
+def split_twitter_text(text, maxchars=140, sep="\n"):
+  def is_url(text):
+    return len(ttp.Parser().parse(text).urls) > 0
+  if (len(text) <= maxchars):
+    return [text]
+  else:
+    # declare locals
+    chunks, words, cid = list(), text.split(sep), 0
+    # loop over all words
+    while (len(words)):
+      cid += 1
+      # add id to each chunk's start
+      chunk = "%d/" % (cid)
+      chunklen = len(chunk)
+      # t.co url's will be truncated to 23 chars and 2 for chunk id
+      maxurllen = 23 + 2
+      # calc length of next word, use twitter-text utils to check if next word is a url
+      nextchunklen = maxurllen if is_url(words[0]) else len(words[0]) + 1
+      # we can add more words to current chunk
+      while (len(words) > 0 and (chunklen + nextchunklen) <= maxchars):
+        # concatenate current word to current chunk
+        chunk = "%s" % (sep).join([chunk, words[0]])
+        chunklen += nextchunklen
+        # remove current word from list
+        words = words[1:]
+        # prepare for next iteration of loop
+        if (len(words) > 0):
+          nextchunklen = maxurllen if is_url(words[0]) else len(words[0]) + 1
+      # got upto 140 chars in the current chunk, add to chunks list and move on
+      chunks.append(chunk)
+  # done with all words, got all chunks, return
+  return chunks
 
 def all_dict_keys(listofdicts):
   return functools.reduce(set.union, map(set, map(dict.keys, listofdicts)))
